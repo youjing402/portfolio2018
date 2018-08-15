e9ea019b6fe75e9697cfc189c85fe094da66d6d3
youjing <onizudango@gmail.com>
2018-08-15 01:08:40 -0400

    router working for HOME page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+	{path: '', component: HomeComponent}
+]
+
+@NgModule({
+  imports: [ RouterModule.forRoot(appRoutes, { enableTracing: true }) ],
+  exports: [ RouterModule ]
+})
+
+export class AppRoutingModule { }
