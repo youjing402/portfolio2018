5e9fb2f2726685beba51bb31b1cb232a6f8ba985
Yucca <yucca@Yuccas-MacBook-Pro.local>
2020-10-20 05:51:09 -0400

    more content added to itale and dh

--- src/app/itale/itale.component.ts
+++ src/app/itale/itale.component.ts
@@ -12,4 +12,9 @@ export class ItaleComponent implements OnInit {
   ngOnInit() {
   }
 
+  scrollToElement($element): void {
+    console.log($element);
+    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
+
 }
