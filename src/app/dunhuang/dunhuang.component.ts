fc91289bd7dc9228524fc90844efc108ae47222a
Yucca <yucca@Yuccas-MacBook-Pro.local>
2020-10-24 04:08:23 -0400

    menu

--- src/app/dunhuang/dunhuang.component.ts
+++ src/app/dunhuang/dunhuang.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
 	selector: 'app-dunhuang',
@@ -6,12 +6,20 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./dunhuang.component.scss']
 })
 export class DunhuangComponent implements OnInit {
-	private isExpanded = false;
+	isNavbarCollapsed = true;
+	isExpanded = false;
 
-	private isSelected1 = true;
-	private isSelected2 = false;
-	private isSelected3 = false;
-	private isSelected4 = false;
+	isSelected1 = true;
+	isSelected2 = false;
+	isSelected3 = false;
+	isSelected4 = false;
+
+	isScrolled1 = false;
+	isScrolled2 = false;
+	isScrolled3 = false;
+	isScrolled4 = false;
+	isScrolled5 = false;
+	isScrolled6 = false;
 
 	constructor() { }
 
@@ -37,6 +45,74 @@ export class DunhuangComponent implements OnInit {
 		} else if (id == 4) {
 			this.isSelected4 = true;
 		}
+	}
+
+	@HostListener('window:scroll', ['$event'])
+	checkScroll() {
+		//const componentPosition = this.el.nativeElement.offsetTop;
+		const scrollPosition = window.pageYOffset;
+		const bodyRectTop = document.body.getBoundingClientRect().top;
+		//const firstScreenRatio = 1/5;
+		const sideScrollPosition = document.getElementsByClassName("banner")[0].clientHeight;
+		const sideScroll2 = document.getElementsByClassName("big-part-2")[0].getBoundingClientRect().top - bodyRectTop;
+		const sideScroll3 = document.getElementsByClassName("big-part-3")[0].getBoundingClientRect().top - bodyRectTop;
+		const sideScroll4 = document.getElementsByClassName("big-part-4")[0].getBoundingClientRect().top - bodyRectTop;
+		const sideScroll5 = document.getElementsByClassName("big-part-5")[0].getBoundingClientRect().top - bodyRectTop;
+		const sideScroll6 = document.getElementsByClassName("big-part-6")[0].getBoundingClientRect().top - bodyRectTop;
+		//var percentage = scrollPosition / maxScrollPosition;
+		//this.blendedColor = this.blend(this.originalColor, "#ff7c7c", percentage);
+		if (scrollPosition >= sideScrollPosition) {
+			this.isScrolled1 = true;
+		}
+		if (scrollPosition < sideScrollPosition) {
+			this.isScrolled1 = false;
+		}
+
+		if (scrollPosition >= sideScroll2) {
+			this.isScrolled2 = true;
+		}
+		if (scrollPosition < sideScroll2) {
+			this.isScrolled2 = false;
+		}
+
+		if (scrollPosition >= sideScroll3) {
+			this.isScrolled3 = true;
+		}
+		if (scrollPosition < sideScroll3) {
+			this.isScrolled3 = false;
+		}
+
+		if (scrollPosition >= sideScroll4) {
+			this.isScrolled4 = true;
+		}
+		if (scrollPosition < sideScroll4) {
+			this.isScrolled4 = false;
+		}
+
+		if (scrollPosition >= sideScroll5) {
+			this.isScrolled5 = true;
+		}
+		if (scrollPosition < sideScroll5) {
+			this.isScrolled5 = false;
+		}
+
+		if (scrollPosition >= sideScroll6) {
+			this.isScrolled6 = true;
+		}
+		if (scrollPosition < sideScroll6) {
+			this.isScrolled6 = false;
+		}
+
+
+		console.log(scrollPosition);
+		console.log("1" + this.isScrolled1);
+		console.log("22" + sideScroll2);
+		console.log("2" + this.isScrolled2);
+	}
+
+	scrollToElement($element): void {
+		console.log($element);
+		$element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
 	}
 
 }
--- src/app/itale/itale.component.ts
+++ src/app/itale/itale.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-itale',
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./itale.component.scss']
 })
 export class ItaleComponent implements OnInit {
+
+  isNavbarCollapsed = true;
+
+  isScreen1 = true;
+  isScreen2 = false;
+  isScreen3 = false;
+  isScreen4 = false;
+
+  isScrolled1 = false;
+  isScrolled2 = false;
+  isScrolled3 = false;
+  isScrolled4 = false;
+  isScrolled5 = false;
+  isScrolled6 = false;
 
   constructor() { }
 
@@ -15,6 +29,86 @@ export class ItaleComponent implements OnInit {
   scrollToElement($element): void {
     console.log($element);
     $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+  }
+
+  selectScreen(id: number) {
+    this.isScreen1 = false;
+    this.isScreen2 = false;
+    this.isScreen3 = false;
+    this.isScreen4 = false;
+
+    if (id == 1) {
+      this.isScreen1 = true;
+    } else if (id == 2) {
+      this.isScreen2 = true;
+    } else if (id == 3) {
+      this.isScreen3 = true;
+    } else if (id == 4) {
+      this.isScreen4 = true;
+    }
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    //const componentPosition = this.el.nativeElement.offsetTop;
+    const scrollPosition = window.pageYOffset;
+    const bodyRectTop = document.body.getBoundingClientRect().top;
+    //const firstScreenRatio = 1/5;
+    const sideScrollPosition = document.getElementsByClassName("banner")[0].clientHeight;
+    const sideScroll2 = document.getElementsByClassName("big-part-2")[0].getBoundingClientRect().top - bodyRectTop;
+    const sideScroll3 = document.getElementsByClassName("big-part-3")[0].getBoundingClientRect().top - bodyRectTop;
+    const sideScroll4 = document.getElementsByClassName("big-part-4")[0].getBoundingClientRect().top - bodyRectTop;
+    const sideScroll5 = document.getElementsByClassName("big-part-5")[0].getBoundingClientRect().top - bodyRectTop;
+    const sideScroll6 = document.getElementsByClassName("big-part-6")[0].getBoundingClientRect().top - bodyRectTop;
+    //var percentage = scrollPosition / maxScrollPosition;
+    //this.blendedColor = this.blend(this.originalColor, "#ff7c7c", percentage);
+    if (scrollPosition >= sideScrollPosition) {
+      this.isScrolled1 = true;
+    }
+    if (scrollPosition < sideScrollPosition) {
+      this.isScrolled1 = false;
+    }
+
+    if (scrollPosition >= sideScroll2) {
+      this.isScrolled2 = true;
+    }
+    if (scrollPosition < sideScroll2) {
+      this.isScrolled2 = false;
+    }
+
+    if (scrollPosition >= sideScroll3) {
+      this.isScrolled3 = true;
+    }
+    if (scrollPosition < sideScroll3) {
+      this.isScrolled3 = false;
+    }
+
+    if (scrollPosition >= sideScroll4) {
+      this.isScrolled4 = true;
+    }
+    if (scrollPosition < sideScroll4) {
+      this.isScrolled4 = false;
+    }
+
+    if (scrollPosition >= sideScroll5) {
+      this.isScrolled5 = true;
+    }
+    if (scrollPosition < sideScroll5) {
+      this.isScrolled5 = false;
+    }
+
+    if (scrollPosition >= sideScroll6) {
+      this.isScrolled6 = true;
+    }
+    if (scrollPosition < sideScroll6) {
+      this.isScrolled6 = false;
+    }
+
+
+    console.log(scrollPosition);
+    console.log("1" + this.isScrolled1);
+     console.log("22" + sideScroll2);
+    console.log("2" + this.isScrolled2);
   }
 
 }
